8eb15d0841fe6907c879337b4c0bb36effd263e3
nk3668 <Nathaniel@157.160.142.21>
2014-09-29 16:56:50 -0400

    Added PersonGateway Barebones NO SQL or DB yet. Have comments at the top to serve as questions/suggestions. Input would be nice.

---
+++ SWE400Project1/src/PersonGateway.java
@@ -0,0 +1,42 @@
+
+
+/**
+ * 
+ * @author Nathaniel
+ *
+ */
+
+
+public class PersonGateway {
+	//Do we want a separate class for the purposes of Finding people in the DB? "PersonFinder" 
+	// Do we need all these instance variables here AND in person? or should person contain a PersonGateway?
+	String username;
+	String password;
+	int userID;
+	String firstName;
+	String lastName;
+	
+	public PersonGateway find(int userID) { 
+		//TODO SQL With JDBC
+		return null;
+	}
+	
+	public PersonGateway findAll() {
+		//TODO SQL With JDBC
+		return null;
+	}
+
+	public void insert() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void delete() {
+		
+	}
+	
+	//get statements for each variable
+}
