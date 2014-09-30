f5ebf5f3ec87b60f0e836a43f3413c686ec3a749
Ethan <Ethan@157.160.169.112>
2014-09-30 11:58:07 -0400

    Added an activity gateway, and updated Activity for now to return void, until we know how to check if something was updated successfully.

--- SWE400Project1/src/Activity.java
+++ SWE400Project1/src/Activity.java
@@ -1,26 +1,30 @@
 import java.util.Date;
+import java.util.List;
 
 
 public class Activity {
 	int userID;
+	int activityID;
 	Date date;
 	String activityType;
 	int caloriesBurned;
+	ActivityGateway act = new ActivityGateway();
 	
 	public void createActivity() {
-
+		act.createActivity(activityID, userID, date, activityType, caloriesBurned);
 	}
 
-	public void readActivity() {
-
+	public List<Activity> readActivity() {
+		List<Activity> activities = act.readActivity(userID);
+		return activities;
 	}
 
-	public boolean updateActivity() {
-		return false;
+	public void updateActivity() {
+		act.updateActivity(activityID, userID, date, activityType, caloriesBurned);
 	}
 
-	public boolean deleteActivity() {
-		return false;
+	public void deleteActivity() {
+		act.deleteActivity(activityID);
 	}
 	
 	
---
+++ SWE400Project1/src/ActivityGateway.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+import java.util.List;
+
+
+public class ActivityGateway {
+
+	public void createActivity(int activityID, int userID, Date date,
+			String activityType, int caloriesBurned) {
+	}
+
+	public List<Activity> readActivity(int userID) {
+		return null;
+	}
+
+	public void updateActivity(int activityID, int userID, Date date,
+			String activityType, int caloriesBurned) {
+	}
+
+	public void deleteActivity(int activityID) {
+	}
+	
+}
