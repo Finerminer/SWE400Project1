f6375434cf861bb95fbfb5623dc4f21f080261fd
gl9859 <gl9859@mct165s07.cs.ship.edu>
2014-10-02 12:26:13 -0400

    Deleted all field variables that resembled domain model deleted comments insert function now has parameters

--- SWE400Project1/src/domainModel/PersonGateway.java
+++ SWE400Project1/src/domainModel/PersonGateway.java
@@ -9,13 +9,8 @@ import java.util.ArrayList;
  *
  */
 public class PersonGateway {
-	//Do we want a separate class for the purposes of Finding people in the DB? "PersonFinder" RYANWEAVER
-	// Do we need all these instance variables here AND in person? or should person contain a PersonGateway?
-	private String username;
-	private String password;
+	
 	private int userID;
-	private String firstName;
-	private String lastName;
 	
 	public Person find(int userID) { 
 		//TODO SQL With JDBC
@@ -27,11 +22,7 @@ public class PersonGateway {
 		return null;
 	}
 
-	public void insert() {
-		
-	}
-	
-	public void update() {
+	public void insert(String username,String password,String firstName,String lastName) {
 		
 	}
 	
@@ -39,40 +30,8 @@ public class PersonGateway {
 		
 	}
 	
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	
 	public void setUserID(int userID){
 		this.userID = userID;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
 	}
 
 	public int getUserID() {
