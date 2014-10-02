0c13fb09bc7935a863892931e9972109d14b93aa
Ethan <Ethan@157.160.78.247>
2014-10-02 16:40:07 -0400

    Added comments to Person Mapper

--- SWE400Project1/src/domainModel/PersonMapper.java
+++ SWE400Project1/src/domainModel/PersonMapper.java
@@ -1,25 +1,50 @@
 package domainModel;
 
+/*
+ * The mapper between Person and PersonGateway. Allows tracking of friends, and finding a Person.
+ */
 public class PersonMapper {
 	protected PersonGateway pGate = new PersonGateway();
 	protected FriendGateway fGate = new FriendGateway(); 
 	
+	/**
+	 * Finds a person based on userID
+	 * @param userID the Persons ID
+	 * @return the Person if one is there.
+	 */
 	public Person find(int userID){
 		if(pGate.find(userID) == null)
 			return null;
 		return pGate.find(userID);
 	}
 	
+	/**
+	 * Inserts a person based on their username, password, and displayName
+	 * @param username the Persons username
+	 * @param password the Persons password
+	 * @param displayName the Persons displayName
+	 * @return if the insert is successful.
+	 */
 	public boolean insertPerson(String username, String password,String displayName){
 		pGate.insert(username, password,displayName);
 		return false;
 	}
 	
+	/**
+	 * Deletes a person based on their userID.
+	 * @param userID the Persons ID
+	 * @return if the delete is successful.
+	 */
 	public boolean deletePerson(int userID){
 		pGate.delete(userID);
 		return false;
 	}
 	
+	/**
+	 * Connects two users into a friend table. 
+	 * @param userID the user who is adding the friend.
+	 * @param friendID the user who is being added.
+	 */
 	public void addFriend(int userID, int friendID){
 		fGate.addFriend(userID, friendID);
 	}
