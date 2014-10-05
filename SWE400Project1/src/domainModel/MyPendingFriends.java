8b162723baa9cf8ae19775d50f313b3d462989b4
Ethan <Ethan@157.160.175.231>
2014-10-05 16:27:25 -0400

    Set up skeleton of pending friends.

--- SWE400Project1/src/domainModel/MyPendingFriends.java
+++ SWE400Project1/src/domainModel/MyPendingFriends.java
@@ -1,5 +1,40 @@
 package domainModel;
 
 public class MyPendingFriends {
+	
+	protected int toUserName;
+	protected int fromUserId;
+	
+	/**
+	 * Gets the user id of the sender
+	 * @return the user ID
+	 */
+	public int getFromUserId() {
+		return fromUserId;
+	}
+	
+	/**
+	 * Sets the user id of the sender
+	 * @param userID
+	 */
+	public void setFromUserId(int userID) {
+		this.fromUserId = userID;
+	}
+	
+	/**
+	 * Gets the user name of the receiver
+	 * @return the user name
+	 */
+	public int getToUserName() {
+		return toUserName;
+	}
+	
+	/**
+	 * Sets the user name of the receiver
+	 * @param pendingFriend
+	 */
+	public void setToUserName(int pendingFriend) {
+		this.toUserName = pendingFriend;
+	}
 
 }
