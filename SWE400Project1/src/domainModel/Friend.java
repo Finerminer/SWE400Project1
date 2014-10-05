ca5a0916a883b0c8d53911dbb8478e605d08aef3
Ethan <Ethan@157.160.175.231>
2014-10-05 15:27:25 -0400

    Updated getters and setters into friend.

--- SWE400Project1/src/domainModel/Friend.java
+++ SWE400Project1/src/domainModel/Friend.java
@@ -1,7 +1,40 @@
 package domainModel;
+
 public class Friend {
 
-	
-	int userID;
-	int friendID;
+	protected int userID;
+	protected int friendID;
+
+	/**
+	 * Gets user ID of the active user.
+	 * @return the main users ID
+	 */
+	public int getUserID() {
+		return userID;
+	}
+
+	/**
+	 * Sets the ID of the active user.
+	 * @param userID
+	 */
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	/**
+	 * Gets the ID of the friend being added.
+	 * @return the friends ID
+	 */
+	public int getFriendID() {
+		return friendID;
+	}
+
+	/**
+	 * Sets the friends ID who is being added.
+	 * @param friendID
+	 */
+	public void setFriendID(int friendID) {
+		this.friendID = friendID;
+	}
+
 }
